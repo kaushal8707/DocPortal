355a20f018d68d0dd9db26a36c56671f1037d3b9
kaushal8707 <kaushal.singh029@gmail.com>
2020-08-13 19:45:26 +0530

    Initial commit

---
+++ src/main/java/com/always/learner/DocPortal/DocPortalApplication.java
@@ -0,0 +1,25 @@
+package com.always.learner.DocPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RibbonClient(name = "doctor-service")
+public class DocPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocPortalApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
+	}
+
+}
---
+++ src/test/java/com/always/learner/DocPortal/DocPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.always.learner.DocPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
